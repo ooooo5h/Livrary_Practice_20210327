cf6bf12a7eac96ac647f998261b9a04c1352a1dd
전은형 <ooooo5h91@gmail.com>
2021-03-27 15:42:48 +0900

    사진 크게보기 화면 추가 => 줌인/줌아웃 불가능한 상태

--- app/src/main/java/com/neppplus/livrary_practice_20210327/MainActivity.kt
+++ app/src/main/java/com/neppplus/livrary_practice_20210327/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.neppplus.livrary_practice_20210327
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        profilePictureImg.setOnClickListener {
+
+//            사진을 크게 보는 화면으로 이동시키자
+
+            val myIntent = Intent(this, ViewProfilePictureActivity::class.java)
+            startActivity(myIntent)
+
+
+        }
     }
 }
---
+++ app/src/main/java/com/neppplus/livrary_practice_20210327/ViewProfilePictureActivity.kt
@@ -0,0 +1,11 @@
+package com.neppplus.livrary_practice_20210327
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewProfilePictureActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_profile_picture)
+    }
+}
