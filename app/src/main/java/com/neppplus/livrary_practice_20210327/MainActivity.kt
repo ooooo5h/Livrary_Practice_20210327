0357fe88569145a2079dc811b8147a31f2b8e955
전은형 <ooooo5h91@gmail.com>
2021-03-27 15:57:42 +0900

    전화 연결 버튼 추가 했는데 권한문제로 앱이 죽는다 ㅠ_ㅠ

--- app/src/main/java/com/neppplus/livrary_practice_20210327/MainActivity.kt
+++ app/src/main/java/com/neppplus/livrary_practice_20210327/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.neppplus.livrary_practice_20210327
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -9,6 +10,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        callBtn.setOnClickListener {
+
+//            버튼이 눌리면 바로 전화 연결
+
+            val myUri = Uri.parse("tel:010-5555-7777")
+
+            val myIntent = Intent(Intent.ACTION_CALL, myUri)
+            startActivity(myIntent)
+        }
+
+
 
         profilePictureImg.setOnClickListener {
 
