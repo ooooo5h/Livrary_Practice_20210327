0a52daca2d16183b092cafc8dc0cc6a5d1d26de9
전은형 <ooooo5h91@gmail.com>
2021-03-27 16:19:17 +0900

    Manifest Call_PHONE 권한 추가 + TedPermission 라이브러리 설치

--- app/src/main/java/com/neppplus/livrary_practice_20210327/MainActivity.kt
+++ app/src/main/java/com/neppplus/livrary_practice_20210327/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.gun0912.tedpermission.PermissionListener
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,7 +14,16 @@ class MainActivity : AppCompatActivity() {
 
         callBtn.setOnClickListener {
 
-//            버튼이 눌리면 바로 전화 연결
+//            버튼이 눌리면 => 권한이 있는지 확인 => OK 이면 바로 전화 연결
+//            권한이 없으면 => 연결 불가 안내 토스트 띄우자
+
+//            권한 상태에 따른 행동 방침을 변수로 저장하자
+
+
+
+//            그 방침을 가지고 => 실제로 권한 확인
+
+
 
             val myUri = Uri.parse("tel:010-5555-7777")
 
