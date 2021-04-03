4adf1a07a35c5f9e99da89cd850f59899b3ddc56
전은형 <ooooo5h91@gmail.com>
2021-04-03 11:17:09 +0900

    Glide로 이미지 불러내기 해보았음 - 인터넷 권한 반드시 필요함

--- app/src/main/java/com/neppplus/livrary_practice_20210327/MainActivity.kt
+++ app/src/main/java/com/neppplus/livrary_practice_20210327/MainActivity.kt
@@ -6,6 +6,7 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.bumptech.glide.Glide
 import com.gun0912.tedpermission.PermissionListener
 import com.gun0912.tedpermission.TedPermission
 import kotlinx.android.synthetic.main.activity_main.*
@@ -15,6 +16,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
+
+//        활동 내역 이미지를 => 인터넷에 있는 이미지를 Glide를 통해서 불러내자
+
+        Glide.with(this).load("https://img.insight.co.kr/static/2019/09/22/700/ar4v04135y3ut73z3jk1.jpg").into(actionImg)
 
         callBtn.setOnClickListener {
 
